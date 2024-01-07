677412796a04f10a7cb4a7279b7474b9445063c0
tugba0278 <tugba.oguz.5428@gmail.com>
2024-01-07 19:10:22 +0300

    step-6 : rapor dosyası eklendi

--- ios_explore_books/lib/services/cloud_database/cloud_constants.dart
+++ ios_explore_books/lib/services/cloud_database/cloud_constants.dart
@@ -1,6 +1,4 @@
 const ownerUserIdFieldName = "user_id";
-const textFieldName = "text";
-const notesCollectionName = "notes";
 const fullNameFieldName = "fullName";
 const genreFieldName = "genre";
 const phoneNumberFieldName = "phoneNumber";
